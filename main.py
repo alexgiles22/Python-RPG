edcc684aa6e509fa862dca6369b4078bc2549c69
Cameron Giles <alex.stump827@gmail.com>
2022-10-03 22:14:58 -0500

    Created my dictionaries for three enemies and the hero.

---
+++ main.py
@@ -0,0 +1,48 @@
+my_hero = {
+    "name": "Aelin"
+    "level": 1
+    "health": 100
+    "equipment": {"Darrens' Sword", "Dagger", "Light Armor"}
+    "attacks": (("Fire Bolt", 25), ("Mind SLiver", 15), ("Aganazzar's Scorcher", 35))
+    "coins": {
+        "copper": 20
+        "silver": 101
+        "gold": 460
+    }
+}
+enemy_one = {
+    "name": "Morty"
+    "level": 1
+    "health": 75
+    "equipment": {"Torn Cape", "Rusted Handaxe"}
+    "attacks": (("Unnarmed Strike", 15), ("Handaxe", 20), ("Acid Spew", 20))
+    "coins": {
+        "copper": 20
+        "silver": 5
+        "gold": 0
+    }
+}
+enemy_two = {
+    "name": "Nomu"
+    "level": 5
+    "health": 100
+    "eqipment": {"Silver Arm Braces", "Brain Matter"}
+    "attacks": (("Mega Punch", 20), ("Eat Brains", 25), ("Flail", 20))
+    "coins": {
+        "copper": 5
+        "silver": 10
+        "gold": 5
+    }
+}
+enemy_three = {
+    "name": "Endeavor"
+    "level": 10
+    "health": 150
+    "equipment": {"Hero Suit", "Lost Family Values", "Guilt"}
+    "attacks": (("Gaslight", 25), ("Shame", 30), ("Family Destruction", 35))
+    "coins": {
+        "copper": 0
+        "silver": 15
+        "gold": 10
+    }
+}
