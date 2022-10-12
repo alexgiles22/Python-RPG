958e1664e911d50ce218fd3718d89aaedfbb7c56
Cameron Giles <alex.stump827@gmail.com>
2022-10-12 17:11:10 -0500

    Made adjustments to the battle logic, added looting function

---
+++ battle.py
@@ -0,0 +1,16 @@
+import random
+import characters
+# hero and enemy attacks to be chosen at random
+h_attack = random.choice(my_hero["attacks"])
+print(h_attack)
+# def battle_one():
+#     for attack in (my_hero["attacks"]):
+
+# def battle_two():
+
+# def battle_three():
+# hero or enemy health to decrease based on the power of a successful attack
+
+# print attack results to termilal
+
+# contimue battle until health reaches 0
